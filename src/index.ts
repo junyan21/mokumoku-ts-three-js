c1b2f792f9bee92e5903b1d987b5d5d018e454fb
Junya <93970227+junyan21@users.noreply.github.com>
2022-03-08 00:19:53 +0900

    Add glTF 3D model via gltfLoader

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as THREE from "three"
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 
 /******************************************************************
@@ -40,14 +41,49 @@ scene.add(camera)
 
 
 /******************************************************************
+ライト
+******************************************************************/
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
+scene.add(directionalLight)
+
+
+/******************************************************************
 空間に3Dオブジェクトを配置します
 ******************************************************************/
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 'green' })
-)
-scene.add(cube)
 
+// Blenderで作ったキャラクターを読み込んで表示します
+const gltfLoader = new GLTFLoader()
+let mixer: THREE.AnimationMixer | null = null
+gltfLoader.load(
+  '../assets/models/mokumoku_blender/zespri.gltf',
+  (gltf) => {
+    gltf.scene.position.y += 2 // 僕のキャラクターは地中に埋まっていたので引き上げています
+    scene.add(gltf.scene)
+    gltf.scene.castShadow = true
+  },
+  (progress) => {
+    console.log(progress)
+  },
+  (error) => {
+    console.log(error)
+  }
+)
+
+// 空間がわかりづらいので平面オブジェクトを置きます
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(20, 20),
+  new THREE.MeshStandardMaterial({
+    color: '#444444',
+    metalness: 0,
+    roughness: 0.5
+  })
+)
+floor.receiveShadow = true
+floor.rotation.x = - Math.PI * 0.5 // y=0の平面へ回転させます
+scene.add(floor)
 
 
 /******************************************************************
@@ -64,8 +100,9 @@ controls.enableDamping = true // 滑らかな慣性スクロールを有効化
 HTMLCanvas要素を要求しているので、最初に取得した'canvas'を入れます
 ******************************************************************/
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+  canvas: canvas,
 })
+renderer.setClearColor(new THREE.Color('white'))
 renderer.setSize(sizes.width, sizes.height)
 // モニタの描画解像度がどんどん上がっているものの肉眼で違いがわかる + パフォーマンス見合い => '2'
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
